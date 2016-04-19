4f450c8e6b9b3d7eb5d805239c77b76157c21ee9
Wiktor Borowski <ulysses128@gmail.com>
2016-04-19 11:36:33 +0200

    1. Zmieniono referencje do entity, teraz referuje na stabilną wersję 7, nie jak poprzednio na jakąś betę. 2. Dodano plik BeerXmlContext.cs

---
+++ src/BeerXML/Models/BeerXmlContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace BeerXML.Models
+{
+    public class BeerXmlContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Water> Waters { get; set; }
+    }
+}
